7832f1e10a85fc6564935838d3f1e62d07b57fe6
ironGriefer <66088066+ironGriefer@users.noreply.github.com>
2021-09-26 14:44:19 -0400

    e
    e

--- buttons.js
+++ buttons.js
@@ -2,8 +2,9 @@
 
 const button1 = document.querySelector('#test1')
 const button2 = document.querySelector('#test2')
+const button3 = document.querySelector('#test3')
 const paragraph = document.querySelector("p")
-let x = new animatedText(beemoviescript, paragraph, 100)
+let x = new animatedText(beemoviescript, paragraph, 'sounds/sans', 10)
 button2.onclick = function() {
 
     
@@ -12,6 +13,10 @@ button2.onclick = function() {
 button1.onclick = function() {
 
     x.stop()
+}
+button3.onclick = function() {
+
+    x.reverse()
 }
 
 
--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 animatedTexts = []
 
+
+class randSound {
+    constructor(folder) {
+        this.folder = folder
+    }
+    play() {
+        var startfile = new Audio(this.folder + "/start.mp3")
+        startfile.play()
+        var sounds = []
+        for (let i = 0; i < 15; i++) {
+            try {
+            var s = new Audio(this.folder + "/randoms/" + i + ".mp3")
+            }
+            catch {
+                i = 20
+            }
+            sounds.push(s)
+            console.log(s)
+
+        }
+        this.soundtime = setInterval(()=>{
+            
+            
+        }, this.speed)
+    
+    }
+}
+
 class animatedText {
-    constructor(text, element, speed) {
+    constructor(text, element, sound, speed) {
         this.element = element
         this.text = text
         this.speed = speed
+        this.sound = sound
         this.time = 0
         this.i = 0
         animatedTexts.push(this)
         
     }
-    setText(settext) {
+    setText(settext, soundfolder) {
         this.text = settext
+        this.sound = sound
         
     }         
     play() {
-        
+        this.stop()
+        var audio = new randSound(this.sound)
+        audio.play()
      //  set your counter to 1
         this.time = setInterval(()=>{
             this.element.innerHTML = this.element.innerHTML + this.text.charAt(this.i)  
             //console.log(self, self.element)
+            
+            
+
             this.i = this.i + 1;    
             
         }, this.speed)
@@ -27,9 +62,18 @@ class animatedText {
             
     }
     stop() {
-        
+        clearInterval(this.soundtime)
         clearInterval(this.time)
     }
-    
+    reverse() {
+        this.stop()
+        this.time = setInterval(()=>{
+            this.element.innerHTML = this.element.innerHTML.substring(0, this.element.innerHTML.length - 1)
+            //console.log(self, self.element)
+            if (this.i > 0) {
+                this.i = this.i - 1;    
+            }
+        }, this.speed)
+    }
 }
 
