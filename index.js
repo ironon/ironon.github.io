502566b18b0904f7d3266637ff89e15f752966aa
ironGriefer <66088066+ironGriefer@users.noreply.github.com>
2022-02-04 12:05:57 -0500

    hi
    just kidding

--- index.js
+++ index.js
@@ -1,79 +1,32 @@
-animatedTexts = []
+var cook = decodeURIComponent(document.cookie)
+var cookie = cook.split(';')
+var all = document.getElementsByTagName("input")
+console.log(cookie)
+
+var welcometext = "this is a \n bug";
 
 
-class randSound {
-    constructor(folder) {
-        this.folder = folder
+const paragraph = document.querySelector("#password")
+let response
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    // when the website loads
+    console.log(all)
+    response = new animatedText(welcometext, paragraph, 'sounds/sans', 70)
+    response.play()
+    for (item of all) {
+        item.addEventListener('keydown', (keyf) => {
+            if (keyf.key == "Enter") {
+                item.blur()
+                
+                response.setPlay(item.value, 0)
+                item.value = ""
+            }
+        }) 
     }
-    play() {
-        var startfile = new Audio(this.folder + "/start.mp3")
-        startfile.play()
-        var sounds = []
-        for (let i = 0; i < 15; i++) {
-            try {
-            var s = new Audio(this.folder + "/randoms/" + i + ".mp3")
-            }
-            catch {
-                i = 20
-            }
-            sounds.push(s)
-            console.log(s)
-
-        }
-        this.soundtime = setInterval(()=>{
-            
-            
-        }, this.speed)
+       
     
-    }
-}
-
-class animatedText {
-    constructor(text, element, sound, speed) {
-        this.element = element
-        this.text = text
-        this.speed = speed
-        this.sound = sound
-        this.time = 0
-        this.i = 0
-        animatedTexts.push(this)
-        
-    }
-    setText(settext, soundfolder) {
-        this.text = settext
-        this.sound = sound
-        
-    }         
-    play() {
-        this.stop()
-        var audio = new randSound(this.sound)
-        audio.play()
-     //  set your counter to 1
-        this.time = setInterval(()=>{
-            this.element.innerHTML = this.element.innerHTML + this.text.charAt(this.i)  
-            //console.log(self, self.element)
-            
-            
-
-            this.i = this.i + 1;    
-            
-        }, this.speed)
-        
-            
-    }
-    stop() {
-        clearInterval(this.soundtime)
-        clearInterval(this.time)
-    }
-    reverse() {
-        this.stop()
-        this.time = setInterval(()=>{
-            this.element.innerHTML = this.element.innerHTML.substring(0, this.element.innerHTML.length - 1)
-            //console.log(self, self.element)
-            if (this.i > 0) {
-                this.i = this.i - 1;    
-            }
-        }, this.speed)
-    }
-}
+});
 
